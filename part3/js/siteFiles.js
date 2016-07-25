7fd0c64db3d644fa096df9b7c288de41b59309e8
Marcos Caceres <marcos@marcosc.com>
2016-07-25 18:02:52 +1000

    fix: load correct site files

--- part3/js/siteFiles.js
+++ part3/js/siteFiles.js
@@ -1,51 +1,71 @@
 /* jshint worker: true */
-/*exports siteFiles */
 // We need to get the files for the site
 // e.g., find . -type f \( ! -iname ".DS_Store" \) -follow -print|xargs ls -l -1
 // Watch for garbage!
+// watch for defaults "index.html"
 
-(function(exports){
-  "use strict";
-  const siteFiles = [
-    // Fill me in
-  ];
-  exports.siteFiles = siteFiles;
-}(self));
+"use strict";
+const siteFiles = [
+  "./",
+  "./favicon.ico",
+  "./images/background_1x.jpg",
+  "./images/background_2x.jpg",
+  "./images/background_2x_crop.jpg",
+  "./images/battery_icon.png",
+  "./images/calendar.svg",
+  "./images/document.svg",
+  "./images/github.svg",
+  "./images/irc.svg",
+  "./images/link.svg",
+  "./images/logos.jpg",
+  "./images/logos.pxm",
+  "./images/mail.svg",
+  "./images/phone.svg",
+  "./images/splash.png",
+  "./images/splash2x.png",
+  "./images/twitter_white.svg",
+  "./images/voip.svg",
+  "./images/w3c_white.svg",
+  "./images/webmob.svg",
+  "./index.html",
+  "./js/accordion.js",
+  "./js/lib/async.js",
+  "./js/registration.js",
+  "./js/siteFiles.js",
+  "./manifest.json",
+  "./styles/fonts/nexa/Nexa_Bold.otf",
+  "./styles/style.css",
+  "./sw.js",
+];
 
-// =====
-// (function(exports){
-//   "use strict";
-//   const siteFiles = [
-//     "./",
-//     "favicon.ico",
-//     "images/background_1x.jpg",
-//     "images/background_2x.jpg",
-//     "images/background_2x_crop.jpg",
-//     "images/battery_icon.png",
-//     "images/calendar.svg",
-//     "images/document.svg",
-//     "images/github.svg",
-//     "images/irc.svg",
-//     "images/link.svg",
-//     "images/logos.jpg",
-//     "images/mail.svg",
-//     "images/phone.svg",
-//     "images/splash.png",
-//     "images/splash2x.png",
-//     "images/twitter_white.svg",
-//     "images/voip.svg",
-//     "images/w3c_white.svg",
-//     "images/webmob.svg",
-//     "js/accordion.js",
-//     "js/registration.js",
-//     "js/lib/async.js",
-//     "js/lib/cachetasks.js",
-//     "js/lib/swMessage.js",
-//     "js/siteFiles.js",
-//     "manifest.json",
-//     "styles/fonts/nexa/Nexa_Bold.otf",
-//     "styles/style.css",
-//   ];
-//   exports.siteFiles = siteFiles;
-// }(self));
-
+/*
+  "./",
+  "./favicon.ico",
+  "./images/background_1x.jpg",
+  "./images/background_2x.jpg",
+  "./images/background_2x_crop.jpg",
+  "./images/battery_icon.png",
+  "./images/calendar.svg",
+  "./images/document.svg",
+  "./images/github.svg",
+  "./images/irc.svg",
+  "./images/link.svg",
+  "./images/logos.jpg",
+  "./images/logos.pxm",
+  "./images/mail.svg",
+  "./images/phone.svg",
+  "./images/splash.png",
+  "./images/splash2x.png",
+  "./images/twitter_white.svg",
+  "./images/voip.svg",
+  "./images/w3c_white.svg",
+  "./images/webmob.svg",
+  "./js/accordion.js",
+  "./js/lib/async.js",
+  "./js/registration.js",
+  "./js/siteFiles.js",
+  "./manifest.json",
+  "./styles/fonts/nexa/Nexa_Bold.otf",
+  "./styles/style.css",
+  "./sw.js",
+*/
