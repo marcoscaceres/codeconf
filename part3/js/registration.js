350e61510091f57cda80a8e6ed5cd60925daed38
Marcos Caceres <marcos@marcosc.com>
2016-07-25 18:17:52 +1000

    feat: registration template

--- part3/js/registration.js
+++ part3/js/registration.js
@@ -1,26 +1,9 @@
 (function() {
   "use strict";
   // Service Worker registration (installation/activation).
-  if (!("serviceWorker" in navigator)) {
-    console.error("No serviceWorker support!");
-    return;
+  function doRegistration() {
+    // Fill me in
   }
 
-  function doRegistration() {
-    async.task(function*() {
-      console.info("Attempting to register SW");
-      let reg = yield navigator.serviceWorker.getRegistration();
-      if (reg) {
-        console.info("Already registered.");
-        return;
-      }
-      console.info("No registration, so register.");
-      try {
-        reg = yield navigator.serviceWorker.register("sw.js");
-      } catch (err) {
-        console.error("SW Registration failed:", err);
-      }
-    }).catch(err => console.err("Something went wrong", err));
-  }
   window.addEventListener("load", doRegistration);
 }());
