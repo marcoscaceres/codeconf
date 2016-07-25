dda567364879c7fdba2344c347a9275f9e473cc1
Marcos Caceres <marcos@marcosc.com>
2016-07-25 18:14:03 +1000

    feat: remove resource list

--- part3/js/siteFiles.js
+++ part3/js/siteFiles.js
@@ -6,36 +6,7 @@
 
 "use strict";
 const siteFiles = [
-  "./",
-  "./favicon.ico",
-  "./images/background_1x.jpg",
-  "./images/background_2x.jpg",
-  "./images/background_2x_crop.jpg",
-  "./images/battery_icon.png",
-  "./images/calendar.svg",
-  "./images/document.svg",
-  "./images/github.svg",
-  "./images/irc.svg",
-  "./images/link.svg",
-  "./images/logos.jpg",
-  "./images/logos.pxm",
-  "./images/mail.svg",
-  "./images/phone.svg",
-  "./images/splash.png",
-  "./images/splash2x.png",
-  "./images/twitter_white.svg",
-  "./images/voip.svg",
-  "./images/w3c_white.svg",
-  "./images/webmob.svg",
-  "./index.html",
-  "./js/accordion.js",
-  "./js/lib/async.js",
-  "./js/registration.js",
-  "./js/siteFiles.js",
-  "./manifest.json",
-  "./styles/fonts/nexa/Nexa_Bold.otf",
-  "./styles/style.css",
-  "./sw.js",
+
 ];
 
 /*
